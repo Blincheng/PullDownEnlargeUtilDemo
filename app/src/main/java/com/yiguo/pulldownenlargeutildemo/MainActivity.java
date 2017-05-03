7949edd75aa6e34b5377ceb8df7ca8080a28dafc
zhaocheng <zhaocheng>
2017-05-03 09:33:07 +0800

    修改最低版本号

--- app/src/main/java/com/yiguo/pulldownenlargeutildemo/MainActivity.java
+++ app/src/main/java/com/yiguo/pulldownenlargeutildemo/MainActivity.java
@@ -1,11 +1,11 @@
 package com.yiguo.pulldownenlargeutildemo;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
